561b9cfd8155287a9618c2133cf1bfb976a146a1
Jioh L. Jung <ziozzang@gmail.com>
2015-06-29 17:31:09 +0900

    Update coreos-python.sh

--- coreos-python.sh
+++ coreos-python.sh
@@ -1,36 +1,13 @@
 #!/bin/bash
 
+# Set Toolbox Env. (Default Toolbox is Fedora. but I prefer Ubuntu.)
 cat > .toolboxrc <<EOF
 TOOLBOX_DOCKER_IMAGE=ubuntu
 TOOLBOX_DOCKER_TAG=14.04
 EOF
 
-toolbox bash -c "apt-get update && apt-get install -fy python"
-toolbox bash -c "apt-get install -fy -qqy curl git-core sudo make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev"
-toolbox bash -c "curl -k -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash"
-
-echo 'export PYENVPATH=$HOME/.pyenv' > ~/.bashrc
-echo 'export PATH=$PYENVPATH/shims:$PYENVPATH/bin:$PATH' >> ~/.bashrc
+# Install busybox-like Toolbox command at /opt/bin/
 mkdir -p /opt/bin
-cat > /opt/bin/python << EOF
-#!/bin/bash
-MOUNTS="/root:/root"
-if [ "\$(id -u)" != "0" ]; then
-  MOUNTS="/home:/home"
-fi
-
-toolbox --bind=\${MOUNTS} python "\$@"
-EOF
-cat > /opt/bin/pip << EOF
-#!/bin/bash
-MOUNTS="/root:/root"
-if [ "\$(id -u)" != "0" ]; then
-  MOUNTS="/home:/home"
-fi
-
-toolbox --bind=\${MOUNTS} pip "\$@"
-EOF
-
 cat > /opt/bin/tbsh << EOF
 #!/bin/bash
 MOUNTS="/root:/root"
@@ -38,10 +15,19 @@ if [ "\$(id -u)" != "0" ]; then
   MOUNTS="/home:/home"
 fi
 
-toolbox --bind=\${MOUNTS} bash "\$@"
+toolbox --bind=\${MOUNTS} --bind=/opt:/opt \${0##*/} "\$@"
 EOF
 
-chmod +x /opt/bin/python
-chmod +x /opt/bin/pip
+# Create Links.
 chmod +x /opt/bin/tbsh
+ln -s /opt/bin/tbsh /opt/bin/python
+ln -s /opt/bin/tbsh /opt/bin/pip
+ln -s /opt/bin/tbsh /opt/bin/pyenv
+ln -s /opt/bin/tbsh /opt/bin/apt-get
+ln -s /opt/bin/tbsh /opt/bin/apt-cache
 
+apt-get update && apt-get install -fy python python-pip
+apt-get install -fy -qqy curl git-core sudo make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev
+
+pip install flask
+# PYENV_ROOT
