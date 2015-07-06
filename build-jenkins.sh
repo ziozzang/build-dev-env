358bb937b375520a249bd267aeffb36605731b1e
Jioh L. Jung <ziozzang@gmail.com>
2015-07-06 11:26:39 +0900

    Update build-jenkins.sh

--- build-jenkins.sh
+++ build-jenkins.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+cat > /etc/apt/sources.list.d/jenkins.list << EOF
+deb http://pkg.jenkins-ci.org/debian binary/
+EOF
+apt-get update && apt-get install -fy jenkins
+service jenkins start
