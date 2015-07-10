5c162bc1bf955816ed485400cb915c92201f8bf2
Jioh L. Jung <ziozzang@gmail.com>
2015-07-10 12:23:01 +0900

    Update build-golang-env-on-ubuntu.sh

--- build-golang-env-on-ubuntu.sh
+++ build-golang-env-on-ubuntu.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+exit 0
 
 sudo apt-get install -fy curl build-essential bison
-
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 # or
 # wget -qO- https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash
-
-
 source ~/.gvm/scripts/gvm
 [[ -s "~/.gvm/scripts/gvm" ]] && source "~/.gvm/scripts/gvm"
 
 gvm install go1.4.2
 gvm use go1.4.2
 go version
+
+
+# PPA Installation
+sudo add-apt-repository -y ppa:gophers/go
+sudo apt-get update
+sudo apt-get install -fy golang-stable
